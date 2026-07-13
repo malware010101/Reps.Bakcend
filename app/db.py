44e879212451bde3d421b3ec10906ff5ecbdd2bf
bryant.gomez <brayan.gomez221098@gmail.com>
2026-07-12 20:28:25 -0600

    movi config.py a db.py y aerich.ini cambie a .db

--- app/db.py
+++ app/db.py
@@ -1,11 +1,21 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = BASE_DIR / "venv" / ".env"
+
+if env_path.exists():
+    load_dotenv(env_path)
+else:
+    load_dotenv()
+
 
 TORTOISE_ORM = {
     "connections": {
-        "default": os.getenv('db_url')
+        "default": os.getenv("db_url")
     },
     "apps": {
         "models": {
